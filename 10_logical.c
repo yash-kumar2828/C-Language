b2bf0088be382973c42e77d36cccf18b580afab0
yash kumar <you@example.com>
2025-07-06 15:40:02 +0530

    logical operator

---
+++ 10_logical.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=0,b=1;
+    printf("the value of a and b is %d\n",a&&b);
+    printf("the value of a or b is %d\n",a||b);
+    printf("the value of not(a) is %d\n",!a);
+    printf("the value of not(b) is %d",!b);
+    return 0;
+}
