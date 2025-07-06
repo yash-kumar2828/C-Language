656085845abf6022584c83af46879ecc232d55e8
yash kumar <you@example.com>
2025-07-06 15:37:21 +0530

    arithmetic

---
+++ 07_arithmetic.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter first number=");
+    scanf("%d",&a);
+    printf("enter second number=");
+    scanf("%d",&b);
+    printf("addition of two number=%d\n",a+b);
+    printf("subtraction of two number=%d\n",a-b);
+    printf("multiplication of two number=%d\n",a*b);
+    printf("division of two number=%d\n",a/b);
+     printf("modulus of two number=%d",a%b);
+    return 0;
+}
