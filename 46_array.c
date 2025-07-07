20ac6528e5590f1feca24b7bb1f5ed18978a63e4
yash kumar <you@example.com>
2025-07-07 11:41:10 +0530

    array

---
+++ 46_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int marks[5];
+    printf("enter 5 marks=\n");
+    for(int i=0;i<5;i++){
+        scanf("%d",&marks[i]);
+    }
+    printf("here are 5 marks =\n");
+    for(int i=0;i<5;i++){
+        printf("the value of marks at index %d is %d\n",i,marks[i]);
+    }
+    return 0;
+}
