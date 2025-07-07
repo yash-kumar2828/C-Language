199292c36104e54657f36120ba591f5b68b76280
yash kumar <you@example.com>
2025-07-07 16:26:44 +0530

    free

---
+++ 75_free.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n;
+    int* ptr;
+    ptr=(int*)calloc(n, sizeof(int));
+    ptr[0]=3;
+    ptr[1]=2;
+    ptr[2]=4;
+    printf("%d\n",ptr[0]);
+    printf("%d\n",ptr[1]);
+    printf("%d\n",ptr[2]);
+    free(ptr);
+    return 0;
+}
